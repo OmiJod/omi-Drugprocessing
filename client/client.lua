bbd2e54d78cf8722fade0728e01bdb7aaff8c3d6
Omi <69292814+OmiJod@users.noreply.github.com>
2022-05-09 00:42:11 +0530

    Removed Unwanted Anim Functions Shifted Server Side

--- client/client.lua
+++ client/client.lua
@@ -1,65 +1,52 @@
-local QBCore = exports['qb-core']:GetCoreObject() 
+local QBCore = exports["qb-core"]:GetCoreObject()
 
 local isProcessing = false
-
+CreateThread(function()
+	Wait(100)
+	QBCore.Functions.TriggerCallback(
+		"qb-drug:server:getConfig",
+		function(config) -- get the info from the server, so no dumping stuff
+			exports["qb-target"]:AddBoxZone("drugprocessing", config.Coords, 2.75, 1, {
+				name = "drugprocessing",
+				heading = 312,
+				debugPoly = false,
+				minZ = 38.75,
+				maxZ = 42.75,
+			}, {
+				options = {
+					{
+						event = "drug:overallproccess",
+						icon = "fas fa-hammer",
+						label = "Process Drug",
+						item = config.ItemToTake, --Change Me on server.lua
+					},
+				},
+				distance = 2,
+			})
+		end
+	)
+end)
 -- Events
 
-RegisterNetEvent('drug:overallproccess', function()
-    local player = GetPlayerPed(-1)
-    FreezeEntityPosition(player, true)
-    TriggerServerEvent('drug:removebrick')
-    playAnim("anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 30000)
-
-    QBCore.Functions.Progressbar("drug-", "Breaking down the drug..", 30000, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true,
-    }, {}, {}, {}, function() -- Done
-        FreezeEntityPosition(player, false)
-        TriggerServerEvent('drug:addbaggies')
-        isProcessing = false
-    end, function() -- Cancel
-        isProcessing = false
-        ClearPedTasksImmediately(player)
-        FreezeEntityPosition(player, false)
-    end)
+RegisterNetEvent("drug:overallproccess", function()
+	local player = PlayerPedId() -- Use this so no loop over all the players
+	FreezeEntityPosition(player, true)
+	QBCore.Functions.Progressbar("drug-", "Breaking down the drug..", 30000, false, true, {
+		disableMovement = true,
+		disableCarMovement = true,
+		disableMouse = false,
+		disableCombat = true,
+	}, {
+		animDict = "anim@amb@clubhouse@tutorial@bkr_tut_ig3@",
+		anim = "machinic_loop_mechandplayer",
+		flags = 16,
+	}, {}, {}, function() -- Done
+		FreezeEntityPosition(player, false)
+		TriggerServerEvent("drug:addbaggies")
+		isProcessing = false
+	end, function() -- Cancel
+		isProcessing = false
+		ClearPedTasksImmediately(player)
+		FreezeEntityPosition(player, false)
+	end)
 end)
-
--- Change This Box Zone Where Ever You want To
-
-exports["qb-target"]:AddBoxZone("drugprocessing", vector3(2430.88, 4971.21, 42.35), 2.75, 1, {
-    name="drugprocessing",
-    heading=312,
-    --debugPoly=true,
-    minZ=38.75,
-    maxZ=42.75
-}, {
-	options = {
-		{
-			event = "drug:overallproccess",     
-			icon = "fas fa-hammer",
-			label = "Process Drug",
-            item = "coke_brick",  --Change Me To The Drug You need To Process For Example coke_brick
-		}
-	},
-	distance = 2
-});
-
-function playAnimPed(animDict, animName, duration, buyer, x,y,z)
-    RequestAnimDict(animDict)
-    while not HasAnimDictLoaded(animDict) do 
-      Citizen.Wait(0) 
-    end
-    TaskPlayAnim(pilot, animDict, animName, 1.0, -1.0, duration, 49, 1, false, false, false)
-    RemoveAnimDict(animDict)
-end
-
-function playAnim(animDict, animName, duration)
-    RequestAnimDict(animDict)
-    while not HasAnimDictLoaded(animDict) do 
-      Citizen.Wait(0) 
-    end
-    TaskPlayAnim(GetPlayerPed(-1), animDict, animName, 1.0, -1.0, duration, 49, 1, false, false, false)
-    RemoveAnimDict(animDict)
-end
