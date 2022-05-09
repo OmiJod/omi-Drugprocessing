e2ca67786df0eba588a4243f88f62a2ae2ce044d
Omi <69292814+OmiJod@users.noreply.github.com>
2022-05-10 00:01:20 +0530

    Improved Log Now Players First Name Will be Displayed

--- server/server.lua
+++ server/server.lua
@@ -25,7 +25,7 @@ RegisterServerEvent("drug:addbaggies", function()
 			TriggerClientEvent("QBCore:Notify", src, "Making Drug Bags", "success", 8000)
 			Player.Functions.AddItem(Config.ItemToGive, 8) -- Lets give the item
 			TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items[Config.ItemToGive], "add") -- Lets give the item
-			TriggerEvent("qb-log:server:CreateLog","default",GetCurrentResourceName(),"default","Drug started by "..Player.PlayerData.citizenid)
+			TriggerEvent("qb-log:server:CreateLog","anticheat",GetCurrentResourceName(),"default","8 bags Of Drug Recieved by "..Player.PlayerData.charinfo.firstname)
 		end
 	else
 		TriggerEvent("qb-log:server:CreateLog","anticheat",GetCurrentResourceName(),"red","Player: "..Player.PlayerData.citizenid.." tried to exploit resource: "..GetCurrentResourceName())
